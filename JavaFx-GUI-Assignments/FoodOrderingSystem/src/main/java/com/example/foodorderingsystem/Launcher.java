fc4147efb6595edb1e85c53d7589fe170c030974
FranceloydTesaluna <franceloyd.tesaluna@cit.edu>
2026-04-03 21:35:53 +0800

    Delete JavaFx-GUI-Assignments directory

--- JavaFx-GUI-Assignments/FoodOrderingSystem/src/main/java/com/example/foodorderingsystem/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.foodorderingsystem;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(FoodApp.class, args);
-    }
-}
